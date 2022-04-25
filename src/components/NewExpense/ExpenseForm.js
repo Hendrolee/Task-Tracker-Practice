96e5c62215bd03e1efb6bab6dc780bf5bd9a5cbf
Hendro Lee <hendrolee39@gmail.com>
2022-04-25 17:27:58 +1000

    added form and listener for user inputs using useState

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const changeTitleHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const changeAmountHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const [enteredDate, setEnteredDate] = useState("");
+  const changeDateHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={changeTitleHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={changeAmountHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={changeDateHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expenses</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
